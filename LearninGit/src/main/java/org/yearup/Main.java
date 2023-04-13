ccc6610b5801a454bb8035b863cc93fb3bfef53e
Gregor Dzierzon <gregor@fullstackdevs.net>
2023-04-13 10:54:32 -0600

    changed code to greet by name

--- LearninGit/src/main/java/org/yearup/Main.java
+++ LearninGit/src/main/java/org/yearup/Main.java
@@ -1,9 +1,16 @@
 package org.yearup;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name = userInput.nextLine();
+
+        System.out.printf("Hello %s! \n", name);
     }
 }
